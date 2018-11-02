aed47342668c2097cea5ab19098b349b96abce35
Angela Duran <duranangela19@gmail.com>
2018-11-02 06:43:04 -0600

    Add feature test for user login

---
+++ spec/features/users/login_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'login process' do
+  it 'allows registered users to log in successfully' do
+    user = create(:user)
+
+    visit '/'
+    click_on 'Log In'
+    expect(current_path).to eq(login_path)
+
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_on 'Log In'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Log Out")
+  end
+#   it 'allows registered users to log out' do
+#     user = User.create(username: 'Kee-gan', password: 'test')
+#
+#     visit '/'
+#     click_on 'Member Log In'
+#     expect(current_path).to eq(login_path)
+#
+#     fill_in :username, with: user.username
+#     fill_in :password, with: user.password
+#
+#     click_on 'Log In'
+#     click_on 'Log Out'
+#
+#     expect(current_path).to eq(root_path)
+#     expect(page).to have_content("Member Log In")
+#   end
+end
