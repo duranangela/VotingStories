f3c2c4581cc5d9a0911d408bad6dd529f17eeb30
Angela Duran <duranangela19@gmail.com>
2018-11-02 08:27:46 -0600

    Add feature test and functionality for logout

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -17,20 +17,20 @@ describe 'login process' do
     expect(page).to have_content("Welcome")
     expect(page).to have_content("Log Out")
   end
-#   it 'allows registered users to log out' do
-#     user = User.create(username: 'Kee-gan', password: 'test')
-#
-#     visit '/'
-#     click_on 'Member Log In'
-#     expect(current_path).to eq(login_path)
-#
-#     fill_in :username, with: user.username
-#     fill_in :password, with: user.password
-#
-#     click_on 'Log In'
-#     click_on 'Log Out'
-#
-#     expect(current_path).to eq(root_path)
-#     expect(page).to have_content("Member Log In")
-#   end
+  it 'allows registered users to log out' do
+    user = create(:user)
+
+    visit '/'
+    click_on 'Log In'
+    expect(current_path).to eq(login_path)
+
+    fill_in :username, with: user.username
+    fill_in :password, with: user.password
+
+    click_on 'Log In'
+    click_on 'Log Out'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Log In")
+  end
 end
