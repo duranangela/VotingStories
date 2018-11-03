6ea2284eea8d252c9b853476b50d9df3a039663d
Angela Duran <duranangela19@gmail.com>
2018-11-03 17:31:16 -0600

    Update favorites controller to search first for existing favorite before creating

--- app/controllers/api/v1/users/favorites_controller.rb
+++ app/controllers/api/v1/users/favorites_controller.rb
@@ -1,12 +1,17 @@
 class Api::V1::Users::FavoritesController < ActionController::API
 
   def create
-    user = User.find(params[:user_id])
-    favorite = user.favorites.create(fave_params)
-    if favorite.save
-      render json: {message: "Favorite created"}
+    fave = Favorite.find_by_url(fave_params[:url])
+    if fave.nil?
+      user = User.find(params[:user_id])
+      favorite = user.favorites.create(fave_params)
+      if favorite.save
+        render json: {message: "Favorite created"}
+      else
+        render json: {message: 'Unable to create favorite'}, status: 400
+      end
     else
-      render json: {message: 'Unable to create favorite'}, status: 400
+      render json: {message: "Favorite already exists"}
     end
   end
 
