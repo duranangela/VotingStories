bd88b64ee96d56fb9de486fa751de6cbc439736f
Angela Duran <duranangela19@gmail.com>
2018-11-03 13:13:34 -0600

    WIP Add checkbox values to faves

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,18 +2,18 @@ let faves = [];
 
 $('input[type=checkbox]').change(function(){
   faves = [];
-  $('.favorites').empty();
   $('input[type=checkbox]:checked').each(function(){
       faves.push(this.value);
   }).get();
 });
 
 $('.faves-button').on('click', function() {
+  console.log(faves)
   $(faves).each(function(fave){
     fetch(`https://localhost:3000/api/v1/users/${current_user_id}/favorites`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "favorites": { "title": `${fave[0]}`, "source": `${fave[1]}`, "url": `${fave[2]}`} })
+      body: JSON.stringify({ "favorite": { "title": `${fave[0]}`, "source": `${fave[1]}`, "url": `${fave[2]}`} })
     })
   })
 })
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe Favorite, type: :model do
     it {should validate_presence_of(:url)}
     it {should validate_uniqueness_of(:url)}
   end
+  describe 'relationships' do
+    it {should belong_to(:user)}
+  end
 end
