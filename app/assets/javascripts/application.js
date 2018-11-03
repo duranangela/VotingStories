6931f895d8a4e0a056b1f78359500acdaad38936
Angela Duran <duranangela19@gmail.com>
2018-11-03 17:21:22 -0600

    Add functionality to post checked titles to favorites

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,14 @@ let faves = [];
 $('input[type=checkbox]').change(function(){
   faves = [];
   $('input[type=checkbox]:checked').each(function(){
-      faves.push(this.value);
+      data = (this.value).split("-&-")
+      faves.push(data);
   }).get();
 });
 
 $('.faves-button').on('click', function() {
-  console.log(faves)
-  $(faves).each(function(fave){
-    fetch(`https://localhost:3000/api/v1/users/${current_user_id}/favorites`, {
+  $.each(faves, function(index, fave){
+    fetch(`http://localhost:3000/api/v1/users/${current_user_id}/favorites`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ "favorite": { "title": `${fave[0]}`, "source": `${fave[1]}`, "url": `${fave[2]}`} })
