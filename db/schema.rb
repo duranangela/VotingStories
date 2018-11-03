a95082269669790054776143dd43c264bb92a09a
Angela Duran <duranangela19@gmail.com>
2018-11-03 09:56:03 -0600

    Add favorites table to db, add model and model tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_025516) do
+ActiveRecord::Schema.define(version: 2018_11_03_155209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.text "title"
+    t.string "source"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:source)}
+    it {should validate_presence_of(:url)}
+    it {should validate_uniqueness_of(:url)}
+  end
+end
